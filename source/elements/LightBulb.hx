6590b74e696f37c28414452d7c20b8cce2237b35
matyas-ivanovic <ime1729@gmail.com>
2015-09-26 16:36:16 -0400

    Does this open a new branch?

--- source/elements/LightBulb.hx
+++ source/elements/LightBulb.hx
@@ -4,12 +4,13 @@ class LightBulb extends Element {
 
   @final private static var MOVE_SPEED = 0;
   @final private static var DEFAULT_SPRITE = ""; //TODO
+  private var direction;
 
 
 /** Constructs a light bulb, light source, with the given level, and initial row and col */
-  public function new(level : PlayState, row : Int, col : Int) {
+  public function new(level : PlayState, row : Int, col : Int, dir: Direction) {
     super(level, row, col, MOVE_SPEED, DEFAULT_SPRITE);
-  }
+    direction=dir;}
 
 
   override public function update() {
