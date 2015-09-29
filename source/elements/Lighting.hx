db51eff5265781e8c4aa4c3187724ed49f31fd73
matyas-ivanovic <ime1729@gmail.com>
2015-09-29 11:17:04 -0400

    IT COMPILES111!

--- source/elements/Lighting.hx
+++ source/elements/Lighting.hx
@@ -1,31 +1,41 @@
 package elements;
 class Lighting
 {
+    private var vertical=2;
+    private var horizontal=1;
     private var light_trace:Array<Array<Int>>;
     public function new(){}
-    public function trace(x,y,direction){
-        Element e=getElementAt(x,y);
+    public function trace(x:Int,y:Int,direction:Direction):Void{
+        var e:Element=GameState.getElementAt(x,y);
         if(e==null){
             light_trace[x][y]+=HV(direction);
             trace(x+direction.x,y+direction.y,direction);}
-        else if(e=="mirror"){
+        else if(Std.is(e,Mirror)){
+            var m:Mirror=Std.instance(e,Mirror);
             if(direction.equals(Direction.Right)){
-                if(e.directionFacing.equals(Direction.Up_Left)){
+                if(m.directionFacing.equals(Direction.Up_Left)){
                     trace(x,y+1,Direction.Up);}
-                else if(e.directionFacing.equals(Direction.Down_Left)){
+                else if(m.directionFacing.equals(Direction.Down_Left)){
                     trace(x,y-1,Direction.Down);}}
             else if(direction.equals(Direction.Left)){
-                if(e.directionFacing.equals(Direction.Up_Left)){
+                if(m.directionFacing.equals(Direction.Up_Left)){
                     trace(x,y-1,Direction.Down);}
-                else if(e.directionFacing.equals(Direction.Down_Left)){
+                else if(m.directionFacing.equals(Direction.Down_Left)){
                     trace(x,y+1,Direction.Up);}}
             else if(direction.equals(Direction.Up)){
-                if(e.directionFacing.equals(Direction.Up_Left)){
+                if(m.directionFacing.equals(Direction.Up_Left)){
                     trace(x+1,y,Direction.Right);}
-                else if(e.directionFacing.equals(Direction.Up_Right)){
+                else if(m.directionFacing.equals(Direction.Up_Right)){
                     trace(x-1,y,Direction.Left);}}
             else if(direction.equals(Direction.Down)){
-                if(e.directionFacing.equals(Direction.Up_Left)){
+                if(m.directionFacing.equals(Direction.Up_Left)){
                     trace(x-1,y,Direction.Left);}
-                else if(e.directionFacing.equals(Direction.Up_Right)){
-                    trace(x+1,y,Direction.Right);}}}}}
+                else if(m.directionFacing.equals(Direction.Up_Right)){
+                    trace(x+1,y,Direction.Right);}}}}
+    private function HV(direction:Direction):Int{
+        if(direction.equals(Direction.Down)||direction.equals(Direction.Up)){
+            return vertical;}
+        else{return horizontal;}}
+    private function light_exists(direction:Direction):Int{
+
+    }}
