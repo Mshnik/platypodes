eefceff2ade984c027909259594f01ac99bcb166
oydang <oyd2@cornell.edu>
2015-09-29 11:20:32 -0400

    implemented getRow() and getCol() in Element.hx

--- source/elements/Element.hx
+++ source/elements/Element.hx
@@ -51,14 +51,14 @@ class Element extends FlxSprite {
       LabelValuePair.weak("velocity", velocity)]);
   }
 
-  /** Return the row of the board this element is currently occupying */
+  /** Return the row of the board this element is currently occupying. The top-left tile is (0,0) */
   public inline function getRow() : Int {
-    return -1;
+    return Std.int(this.y / level.tileHeight);
   }
 
-  /** Return the col of the board this element is currently occupying */
+  /** Return the col of the board this element is currently occupying. The top-left tile is (0,0) */
   public inline function getCol() : Int {
-    return -1;
+    return Std.int(this.x / level.tileWidth);
   }
 
   /** Sets the movement direction of this element.
@@ -99,6 +99,10 @@ class Element extends FlxSprite {
 
     var newRow = getRow();
     var newCol = getCol();
+
+    trace(" WHEEEEEEEEEEEE");
+    trace(newCol);
+    trace(newRow);
 
     if (oldRow != newRow || oldCol != newCol) {
       //level.elementMoved(this, oldRow, oldCol);
