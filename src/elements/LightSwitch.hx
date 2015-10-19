5e6f9883be770f61bc5f53f68c09db791d56d20b
MShnik <Mgpshnik@gmail.com>
2015-10-19 17:47:54 -0400

    Fixed light sprites being off, now uses gray orb for switch:

--- src/elements/LightSwitch.hx
+++ src/elements/LightSwitch.hx
@@ -2,14 +2,18 @@ package elements;
 import flixel.addons.editors.tiled.TiledObject;
 class LightSwitch extends Element implements Lightable {
 
-  @final private static var DEFAULT_SPRITE = AssetPaths.light_orb__png;
+  @final private static var DEFAULT_SPRITE = AssetPaths.light_orb_grey__png;
 
-  public var isLit(default, default) : Bool;
+  public var isLit(default, set) : Bool;
 
   /** Constructs a new light switch, with the given level, and initial row and col */
   public function new(state : GameState, o : TiledObject) {
     super(state, o, DEFAULT_SPRITE);
     isLit = false;
+  }
+
+  public function set_isLit(isLit : Bool) : Bool {
+
   }
 
   override public function update() {
