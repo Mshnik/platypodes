de241ee8c89fb54c6ea052b9b87a190b7f3aebfd
jeherrera <jeherrera@users.noreply.github.com>
2015-10-19 15:53:41 -0400

    pushpull keys and p/p down anim added

--- src/elements/Character.hx
+++ src/elements/Character.hx
@@ -32,6 +32,12 @@ class Character extends MovingElement {
   
   /** The grab down animation key */
   public inline static var GRAB_DOWN_KEY = "Grab Down";
+  
+  /** The push/pull down animation key */
+  public inline static var PUSH_PULL_LEFT_RIGHT_KEY = "Push-Pull Left-Right";
+  
+  /** The push/pull down animation key */
+  public inline static var PUSH_PULL_DOWN_KEY = "Push-Pull Down";
 
   /** The death animation key */
   public inline static var DEATH_ANIMATION_KEY = "Die";
@@ -94,6 +100,7 @@ class Character extends MovingElement {
     animation.add(WALK_DOWN_KEY, [0,1,2,3], 20, false);
     animation.add(GRAB_LEFT_RIGHT_KEY, [24,25,26,27], 20, false);
     animation.add(GRAB_DOWN_KEY, [16,17,18,19], 20, false);
+    animation.add(PUSH_PULL_DOWN, [20,21,22,23], 20, false);
 
     var arr : Array<Int> = Main.rangeToArray(32, 40);
     arr.push(4);
