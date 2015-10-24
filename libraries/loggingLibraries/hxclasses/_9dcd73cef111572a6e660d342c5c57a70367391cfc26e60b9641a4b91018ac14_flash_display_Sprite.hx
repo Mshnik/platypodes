25a2ac5bb57d8e84eb3a80b544dcedde9b9ffcd3
MShnik <Mgpshnik@gmail.com>
2015-10-24 18:51:39 -0400

    Got library to be included, but seems to be buggy for actually running

--- libraries/loggingLibraries/hxclasses/_9dcd73cef111572a6e660d342c5c57a70367391cfc26e60b9641a4b91018ac14_flash_display_Sprite.hx
+++
@@ -1,5 +0,0 @@
-extern class _9dcd73cef111572a6e660d342c5c57a70367391cfc26e60b9641a4b91018ac14_flash_display_Sprite extends flash.display.Sprite {
-	function new() : Void;
-	function allowDomainInRSL(?p1 : Dynamic, ?p2 : Dynamic, ?p3 : Dynamic, ?p4 : Dynamic, ?p5 : Dynamic) : Void;
-	function allowInsecureDomainInRSL(?p1 : Dynamic, ?p2 : Dynamic, ?p3 : Dynamic, ?p4 : Dynamic, ?p5 : Dynamic) : Void;
-}
