4df66480b5b3e8ead67efc8a8629f2d322e368fb
MShnik <Mgpshnik@gmail.com>
2015-11-01 01:25:57 -0500

    Added some new levels

--- src/LevelSelectMenuState.hx
+++ src/LevelSelectMenuState.hx
@@ -26,6 +26,8 @@ class LevelSelectMenuState extends FlxState
 
     levels = new Array<Dynamic>();
     levels.push(AssetPaths.l0__tmx);
+    levels.push(AssetPaths.l1__tmx);
+    levels.push(AssetPaths.l2__tmx);
 
     var x = MARGIN;
     var y = MARGIN;
