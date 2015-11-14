676bcfc9e088cfe08ad84cd192c6797149e8ee65
MShnik <Mgpshnik@gmail.com>
2015-11-14 17:32:20 -0500

    Fixed assets, hopefully

--- src/AssetPaths.hx
+++ src/AssetPaths.hx
@@ -1,12 +1,12 @@
 package;
 
-@:build(flixel.system.FlxAssets.buildFileReferences("assets/images/friends_release", true))
-@:build(flixel.system.FlxAssets.buildFileReferences("assets/levels/friends_release", true))
+@:build(flixel.system.FlxAssets.buildFileReferences("assets/images/newgrounds_release", true))
+@:build(flixel.system.FlxAssets.buildFileReferences("assets/levels/newgrounds_release", true))
 @:build(flixel.system.FlxAssets.buildFileReferences("assets/music", true))
 @:build(flixel.system.FlxAssets.buildFileReferences("assets/sounds", true))
 class AssetPaths {
 
   /** The current root for images. Should change by version */
-  @final static public var IMAGE_ROOT = "assets/images/newgrounds_release/";
+  @final static public var IMAGE_ROOT = "assets/images/frineds_release/";
 
 }
