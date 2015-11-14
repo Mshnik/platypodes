03582e8ddf4c03ad220a15c3ae0b5fb1be2b8a66
MShnik <Mgpshnik@gmail.com>
2015-11-14 12:27:22 -0500

    Figured out how to only build asset paths for part of the project, got rid of frustrating naming conventions

--- src/AssetPaths.hx
+++ src/AssetPaths.hx
@@ -1,9 +1,12 @@
 package;
 
-@:build(flixel.system.FlxAssets.buildFileReferences("assets", true))
+@:build(flixel.system.FlxAssets.buildFileReferences("assets/images/friends_release", true))
+@:build(flixel.system.FlxAssets.buildFileReferences("assets/levels/friends_release", true))
+@:build(flixel.system.FlxAssets.buildFileReferences("assets/music", true))
+@:build(flixel.system.FlxAssets.buildFileReferences("assets/sounds", true))
 class AssetPaths {
 
   /** The current root for images. Should change by version */
-  @final static public var IMAGE_ROOT = "assets/images/friends_release/";
+  @final static public var IMAGE_ROOT = "assets/images/newgrounds_release/";
 
 }
