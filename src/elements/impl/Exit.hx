c4da6165a874643e0a2fdda9073b3e5dc6d5452d
MShnik <Mgpshnik@gmail.com>
2015-11-16 00:07:52 -0500

    Fixed collision sound to only play once per thing you collied to, resets after moving. Added exit open close sound, fixed tutorial level to not be pre-solved

--- src/elements/impl/Exit.hx
+++ src/elements/impl/Exit.hx
@@ -1,4 +1,6 @@
 package elements.impl;
+import flixel.FlxG;
+import flixel.system.FlxSound;
 import flixel.addons.editors.tiled.TiledObject;
 
 class Exit extends Element {
@@ -10,6 +12,8 @@ class Exit extends Element {
 
   public var isOpen(default, set) : Bool;
 
+  private var openCloseSound : FlxSound;
+
 /** Constructs an exit, with the given level, and initial row and col */
   public function new(level : GameState, o : TiledObject) {
     super(level, o);
@@ -17,6 +21,8 @@ class Exit extends Element {
     loadGraphic(AssetPaths.gate_sheet__png, true, PMain.SPRITE_SIZE, PMain.SPRITE_SIZE);
     animation.add(OPEN_ANIMATION_KEY, [0,2,4,5,6,8], ANIMATION_SPEED, false);
     animation.add(CLOSE_ANIMATION_KEY, [8,6,5,4,2,0], ANIMATION_SPEED, false);
+
+    openCloseSound = FlxG.sound.load(AssetPaths.gateOpenClose__wav, 1.0);
   }
 
   public function set_isOpen(open : Bool) : Bool {
@@ -26,6 +32,7 @@ class Exit extends Element {
       } else {
         animation.play(CLOSE_ANIMATION_KEY);
       }
+      openCloseSound.play(true);
     }
     return this.isOpen = open;
   }
